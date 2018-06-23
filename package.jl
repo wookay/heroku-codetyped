bff92a7e0b5f8bfc16c001700d1e03e2072fb6d8
WooKyoung Noh <wookay.noh@gmail.com>
2018-06-24 04:55:24 +0900

    package

--- package.jl
+++ package.jl
@@ -6,4 +6,13 @@ Pkg.REPLMode.pkgstr("add Documenter#master")
 Pkg.REPLMode.pkgstr("add AbstractTrees#master")
 Pkg.REPLMode.pkgstr("add https://github.com/wookay/DataLogger.jl#master")
 Pkg.REPLMode.pkgstr("add https://github.com/wookay/Poptart.jl#master")
-using Bukdu
+
+import InteractiveUtils: @code_typed
+import Core.Compiler: IRCode
+import Poptart: groupwise_codestack
+import AbstractTrees: print_tree
+using Millboard # table
+using DataLogger
+using Bukdu # ApplicationController Changeset plug get post
+using Bukdu.HTML5.Form # change form_for text_input
+import Documenter.Utilities.DOM: @tags
