48b4291df0a4756ece06e19586dedc05cb292d73
WooKyoung Noh <wookay.noh@gmail.com>
2018-07-07 16:21:37 +0900

    update package

--- package.jl
+++ package.jl
@@ -1,16 +1,16 @@
 using Pkg
-Pkg.REPLMode.pkgstr("add HTTP#master")
-Pkg.REPLMode.pkgstr("add https://github.com/wookay/Bukdu.jl.git#sevenstars")
-Pkg.REPLMode.pkgstr("add https://github.com/wookay/Millboard.jl.git#master")
-Pkg.REPLMode.pkgstr("add Documenter#master")
-Pkg.REPLMode.pkgstr("add AbstractTrees#master")
+Pkg.REPLMode.pkgstr("add HTTP")
+Pkg.REPLMode.pkgstr("add Bukdu")
+Pkg.REPLMode.pkgstr("add Millboard")
+Pkg.REPLMode.pkgstr("add Documenter")
+Pkg.REPLMode.pkgstr("add AbstractTrees")
 Pkg.REPLMode.pkgstr("add https://github.com/wookay/DataLogger.jl#master")
 Pkg.REPLMode.pkgstr("add https://github.com/wookay/Poptart.jl#master")
 
 import InteractiveUtils: @code_typed
 import Core.Compiler: IRCode
-import Poptart: groupwise_codestack
-import AbstractTrees: print_tree
+import Poptart
+import AbstractTrees: print_tree, printnode
 using Millboard # table
 using DataLogger
 using Bukdu # ApplicationController Changeset plug get post
