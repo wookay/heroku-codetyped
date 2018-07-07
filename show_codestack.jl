0f03ba2a242e4b0e8b31206409ac1e479b433a56
WooKyoung Noh <wookay.noh@gmail.com>
2018-07-07 15:57:02 +0900

    update print_tree

--- show_codestack.jl
+++ show_codestack.jl
@@ -9,8 +9,8 @@
 
 import InteractiveUtils: @code_typed
 import Core.Compiler: IRCode
-import Poptart: groupwise_codestack
-import AbstractTrees: print_tree
+import Poptart
+import AbstractTrees: print_tree, printnode
 using Millboard # table
 using DataLogger
 
@@ -33,23 +33,35 @@ function layout(body)
   <title>@code_typed 🏂</title>
   <style>
 label, input, span.prompt {
-  font-size: 2em;
-  font-family: Monospace;
+    font-size: 1.6em;
+    font-family: Monospace;
 }
 span.prompt {
-  color: green;
+    color: green;
 }
 input {
-  padding: 0.2em;
+    padding: 0.2em;
 }
 pre {
-  font-size: 1.5em;
+    font-size: 1.3em;
 }
 .form_submit {
-  margin-left: 10px;
-  font-size: 2em !important;
+    margin-left: 10px;
+    font-size: 2em !important;
+}
+div#examples {
+    padding-bottom: 10px;
+}
+a.example {
+    padding-left: 20px;
+    text-decoration: none;
 }
   </style>
+  <script>
+function clicked_example(data) {
+    document.getElementById('codedata_data').value = data;
+}
+  </script>
 </head>
 <body>
 $body
@@ -81,18 +93,29 @@ function top_function()
     k()
 end
 
+function printnode(io::IO, node::Poptart.Node{<:NamedTuple})
+    entry = node.data.value
+    if entry === nothing
+        Base.printstyled(io, "🌲")
+    else
+        Base.printstyled(io, entry.method, color=:blue)
+    end
+end
+
 function show_codestack(data)
     io = IOBuffer()
     expr = Meta.parse(data)
     if expr isa Expr
         try
             (src,) = eval(Expr(:macrocall, Symbol("@code_typed"), LineNumberNode(1, :none), expr))
-            code = Core.Compiler.inflate_ir(src, Core.svec())::IRCode
-            codestack = groupwise_codestack(code)
+            code = Core.Compiler.inflate_ir(src)::IRCode
+            codestack = Poptart.groupwise_codestack(code)
             println(io)
-            println(io, table(codestack.A))
+            println(io, code)
             println(io)
             print_tree(io, codestack.tree)
+            println(io)
+            println(io, table(codestack.A))
         catch ex
             println(io, ex)
         end
@@ -108,7 +131,7 @@ function index(c::IRController, show_banner=true)
     if !isempty(result.changes)
         changeset.changes = merge(changeset.changes, result.changes)
     end
-    @tags div label span pre a
+    @tags div label span pre a ul li
     form1 = form_for(changeset, (IRController, post_result), method=post, multipart=true) do f
         div(
             span[:class => "prompt"]("julia> "),
@@ -121,10 +144,12 @@ function index(c::IRController, show_banner=true)
     top = div(a[:href => "/"]("Poptart"))
     banner_help = show_banner ? div(
         pre(DataLogger.read_stdout(Base.banner)),
-        pre[:style => "background-color: #e3e3e3"]("""Examples:
-            2pi
-            length("abc")
-        """)
+        div[:id => "examples"](
+            span("Examples:"),
+            div(a[:class => "example", :href => "#", :onclick => """clicked_example('2pi')"""]("""2pi""")),
+            div(a[:class => "example", :href => "#", :onclick => """clicked_example('length("abc")')"""]("""length("abc")""")),
+            div(a[:class => "example", :href => "#", :onclick => """clicked_example('sin(deg2rad(90))')"""]("""sin(deg2rad(90))""")),
+        )
     ) : ""
     source_link = div(a[:href => "https://github.com/wookay/Poptart.jl/blob/master/examples/parallax/show_codestack.jl"]("source code"))
     body = div(top, banner_help, form1, pre(show_codestack(changeset.changes.data)), source_link)
